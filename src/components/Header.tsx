d8a52c9beb957eb15da56bbb12b51cea7a862c8a
Hkaar <HkaarDev@gmail.com>
2024-11-28 22:52:56 +0800

    Updated existing components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 
-import { useState, useEffect } from "react";
+import React from "react";
+import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
@@ -13,25 +14,63 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 import ThemeToggle from "@/components/ThemeToggle";
+import {
+  Calculator,
+  Calendar,
+  Command as CommandIcon,
+  Compass,
+  CreditCard,
+  NotepadText,
+  Settings,
+  Smile,
+  User,
+} from "lucide-react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
 
 interface Props {
   className?: string;
 }
 
+type SubMenu = {
+  icon?: React.ReactNode;
+  title: string;
+  description: string;
+  link: string;
+};
+
+const recipeSubMenus: SubMenu[] = [
+  {
+    icon: <Compass className="size-4" />,
+    title: "Browse",
+    description:
+      "Browse the catalogue of cooking recipes that we provided for you to use",
+    link: "/recipes",
+  },
+  {
+    icon: <NotepadText className="size-4" />,
+    title: "Collections",
+    description:
+      "Explore our collections of recipes that are related to one another",
+    link: "/recipes/collections",
+  },
+];
+
 export default function Header(props: Props) {
   const [searchOpen, setSearchOpen] = useState(false);
 
   const handleKeyPress = (event: KeyboardEvent) => {
-    if (event.ctrlKey && event.key === 'k') {
+    if (event.ctrlKey && event.key === "k") {
       event.preventDefault();
       setSearchOpen(true);
     }
@@ -42,13 +81,13 @@ export default function Header(props: Props) {
 
     return () => {
       document.removeEventListener("keydown", handleKeyPress);
-    }
-  }, [])
+    };
+  }, []);
 
   return (
     <header
       className={cn(
-        "w-full px-4 py-3 border-b border-gray-200 dark:border-neutral-700 sticky top-0",
+        "w-full px-4 py-3 border-b bg-background border-gray-200 dark:border-neutral-700 sticky top-0 z-40",
         props.className,
       )}
     >
@@ -65,28 +104,39 @@ export default function Header(props: Props) {
                   className={navigationMenuTriggerStyle()}
                   href="/"
                 >
-                  About us
+                  Home
                 </NavigationMenuLink>
               </NavigationMenuItem>
 
               <NavigationMenuItem>
                 <NavigationMenuTrigger>Recipes</NavigationMenuTrigger>
 
-                <NavigationMenuContent className="min-w-96 h-64">
-                  <NavigationMenuLink>Link</NavigationMenuLink>
+                <NavigationMenuContent>
+                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                    {recipeSubMenus.map((menu) => (
+                      <ListItem
+                        key={menu.title}
+                        icon={menu.icon}
+                        title={menu.title}
+                        href={menu.link}
+                      >
+                        {menu.description}
+                      </ListItem>
+                    ))}
+                  </ul>
                 </NavigationMenuContent>
               </NavigationMenuItem>
 
               <NavigationMenuItem>
                 <NavigationMenuTrigger>Documentation</NavigationMenuTrigger>
-                <NavigationMenuContent className="min-w-96 h-64">
+                <NavigationMenuContent className="p-4 md:w-[400px] lg:w-[500px]">
                   <NavigationMenuLink>Link 2</NavigationMenuLink>
                 </NavigationMenuContent>
               </NavigationMenuItem>
 
               <NavigationMenuItem>
                 <NavigationMenuTrigger>Community</NavigationMenuTrigger>
-                <NavigationMenuContent className="min-w-96 h-64">
+                <NavigationMenuContent className="p-4 md:w-[400px] lg:w-[500px]">
                   <NavigationMenuLink>Link 3</NavigationMenuLink>
                 </NavigationMenuContent>
               </NavigationMenuItem>
@@ -95,29 +145,20 @@ export default function Header(props: Props) {
         </div>
 
         <div className="flex items-center gap-3">
-          <Button variant={"outline"} className="flex items-center gap-5" onClick={() => setSearchOpen(true)}>
+          <Button
+            variant={"outline"}
+            className="flex items-center gap-5"
+            onClick={() => setSearchOpen(true)}
+          >
             <span className="text-sm text-gray-500 dark:text-neutral-600">
-              Search for a recipe...
+              Search website...
             </span>
 
             <Badge
-              className="p-1 flex items-center gap-2"
+              className="p-1 flex items-center gap-2 text-gray-700 dark:text-neutral-300"
               variant={"outline"}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z"
-                />
-              </svg>
+              <CommandIcon className="size-4" />
               CTRL+K
             </Badge>
           </Button>
@@ -127,16 +168,81 @@ export default function Header(props: Props) {
       </div>
 
       <Dialog open={searchOpen} onOpenChange={setSearchOpen}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </DialogDescription>
-          </DialogHeader>
+        <DialogContent hideClose className="p-0">
+          <Command className="rounded-lg md:min-w-[450px]">
+            <CommandInput placeholder="Type a command or search..." />
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandGroup heading="Suggestions">
+                <CommandItem>
+                  <Calendar />
+                  <span>Calendar</span>
+                </CommandItem>
+                <CommandItem>
+                  <Smile />
+                  <span>Search Emoji</span>
+                </CommandItem>
+                <CommandItem disabled>
+                  <Calculator />
+                  <span>Calculator</span>
+                </CommandItem>
+              </CommandGroup>
+              <CommandSeparator />
+              <CommandGroup heading="Settings">
+                <CommandItem>
+                  <User />
+                  <span>Profile</span>
+                  <CommandShortcut>⌘P</CommandShortcut>
+                </CommandItem>
+                <CommandItem>
+                  <CreditCard />
+                  <span>Billing</span>
+                  <CommandShortcut>⌘B</CommandShortcut>
+                </CommandItem>
+                <CommandItem>
+                  <Settings />
+                  <span>Settings</span>
+                  <CommandShortcut>⌘S</CommandShortcut>
+                </CommandItem>
+              </CommandGroup>
+            </CommandList>
+          </Command>
         </DialogContent>
       </Dialog>
     </header>
   );
 }
+
+type ListItemProps = React.ComponentPropsWithoutRef<"a"> & {
+  icon?: React.ReactNode;
+};
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  ListItemProps
+>(({ className, title, children, icon, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className,
+          )}
+          {...props}
+        >
+          <div className="flex items-center gap-2">
+            {icon}
+            <div className="text-sm font-medium leading-none">{title}</div>
+          </div>
+
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
